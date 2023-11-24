029953fd1d242f0263c7c968fcffb05506673c95
BolotbekovvB <149691592+BolotbekovvB@users.noreply.github.com>
2023-11-25 00:24:44 +0600

    Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Boss {
+public class Boss extends GameEntity {
+    public String printInfo(){
+        return "Health: " + getHealth() + " Damage: " + getDamage() + " Weapon Type: " + getWeapon().getWeapon() +
+                " Weapon Name: " + getWeapon().getWeaponName();
+    }
+    Weapon weapon = new Weapon();
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
 }
